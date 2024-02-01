d467b88faf2a8fdd39cf2b82d7201d4cd8677bb4
AnkitaDes <desai_ankita@outlook.com>
2024-01-31 22:48:08 -0500

    add server setup

---
+++ Server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5002;
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log(`MongoDB connected`))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
